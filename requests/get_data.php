21891b555b784b9bda08c99aaacca4c63fccce05
Turuntuntun <43621680+Turuntuntun@users.noreply.github.com>
2020-11-23 23:23:21 +0300

    Добавлени файла обработки запроса

---
+++ requests/get_data.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Юра
+ * Date: 22.11.2020
+ * Time: 17:19
+ */
+
+use classes\request;
+use classes\data;
+require '..\classes\request.php';
+require '..\classes\data.php';
+
+$request = new request();
+$data = $request->getData();
+$id = $_POST['id'];
+$type = $_POST['type'];
+
+$data = data::getResult($data,$type,$id);
+echo json_encode($data);
